029951bd32d102f9057933af6d6508decf3bfc80
Bernd Mueller <bernd.mueller5@web.de>
2026-07-28 15:44:03 +0200

    final fixes on style.css

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    plugins: [tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        pokedex: resolve(__dirname, 'pokedex.html'),
+      },
+    },
+  },
 });
